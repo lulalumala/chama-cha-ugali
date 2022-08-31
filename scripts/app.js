a0ec539caa497b5fbcb7097625694d114ba3feed
lulalumala <stephenlumala@gmail.com>
2022-08-31 17:07:10 +0300

    started working on js

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,76 @@
+const emailInput = document.querySelector(".email-address")
+const passwordAddress = document.querySelector(".password__address")
+const login = document.querySelector(".login")
 const emailAddress = document.querySelector(".email-input")
 const password = document.querySelector(".password-input")
 const confirmPassword = document.querySelector(".confirm-password")
 const submit = document.querySelector(".submit")
-console.log(emailAddress)
-emailAddress.addEventListener("input", function () {
-    console.log(emailAddress.value)
+const letters = "abcdefghijklmnopqrstuvwxyz"
+const upperCase = letters.toUpperCase()
+const specialChar = "!@#$%^&*()<>;?"
+const upper = []
+const lower = []
+const special = []
+const num = []
+const passwordValue = emailInput.value
+// innertext
+const eAddress = document.querySelector(".eaddress")
+const passwordLabel = document.querySelector(".pword")
+const confirmPasswrd=document.querySelector(".confirm")
+
+
+
+submit.addEventListener("click", function (e) {
+    console.log(password.value.length)
+    if (emailAddress.value == "") {
+        eAddress.innerHTML="Please enter your email address"
+        console.log("Please enter")
+    }
+   
+    if (password.value.length < 8 || password.value.length > 12) {
+        passwordLabel.innerHTML= "Your password should be between 8 to 12 characters"
+        console.log("Your password should be between 8 to 12 characters")
+    }
+    if (password !== confirmPassword) {
+        confirmPasswrd.innerHTML="Your password does not match"
+        console.log("Your password doesn't match")
+    }
+    
+        for (char of password.value) {
+            if (letters.includes(char)) {
+                lower.push(char)
+            }
+        
+            else if (upperCase.includes(char)) {
+                upper.push(char)
+            }
+            else if (specialChar.includes(char)) {
+                special.push(char)
+            }
+            else if (Number(char) !== NaN) {
+                num.push(char)
+            }
+        
+        console.log(lower)
+        console.log(upper)
+            console.log(special)
+            console.log(num)
+            if (upper.length == 0) {
+                password.value=null
+               passwordLabel.innerHTML= "Your password must have a least one capital letter"
+            }
+            else if (lower.length == 0) {
+                password.value=null
+                passwordLabel.innerHTML= "Your password must have at least one lower case letter"
+            }
+            else if (special.length == 0) {
+                password.value=null
+                passwordLabel.innerHTML= "Your password must have at least one special character" 
+            }
+            else if (num.length == 0) {
+                password.value=null
+                passwordLabel.innerHTML="Your password must have at least one numer"
+            }
+               
+    }
 })
